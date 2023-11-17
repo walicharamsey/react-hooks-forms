8bbd56896b29687b8e7a938837a6d157a9b1fe10
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-11-17 18:45:14 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,26 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input
+        type="text"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
       <button type="submit">Submit</button>
     </form>
   );
